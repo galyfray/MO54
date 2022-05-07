2d18ca208eadca975e5a4e0800a79991666cacf7
Galyfray <galyfray@gmail.com>
2022-05-07 18:07:04 +0200

    dummy server setup

---
+++ Server/JS/get/dummy.js
@@ -0,0 +1,13 @@
+const INDEX = require("../utils/Index.js");
+
+INDEX.add(
+    {
+        "type": "get",
+        "handler": (req, res) => {
+            res.status(200).json({
+                message: "dummy"
+            });
+        },
+        "route": "/dummy"
+    }
+);
